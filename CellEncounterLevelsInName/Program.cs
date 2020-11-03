b931be960a04a39c20e7a4b3e258b5f29221c124
Deigue <deigue@hotmail.com>
2020-11-03 18:21:04 -0500

    Get all base information required for patcher.

--- CellEncounterLevelsInName/Program.cs
+++ CellEncounterLevelsInName/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Mutagen.Bethesda;
 using Mutagen.Bethesda.Synthesis;
 using Mutagen.Bethesda.Skyrim;
+using Noggog;
 
 namespace CellEncounterLevelsInName
 {
@@ -13,12 +14,46 @@ namespace CellEncounterLevelsInName
         {
             return SynthesisPipeline.Instance.Patch<ISkyrimMod, ISkyrimModGetter>(
                 args: args,
-                patcher: RunPatch);
+                patcher: RunPatch,
+                new UserPreferences()
+                {
+                    ActionsForEmptyArgs = new RunDefaultPatcher
+                    {
+
+                        IdentifyingModKey = "CellEncounterLevelsInName.esp",
+                        TargetRelease = GameRelease.SkyrimSE
+                    }
+                }
+            );
         }
 
         public static void RunPatch(SynthesisState<ISkyrimMod, ISkyrimModGetter> state)
         {
-            //Your code here!
+            bool debugMode = true;
+
+            Console.WriteLine("Running Cell Encounter Levels In Name ...");
+            int cellCounter = 0;
+            ILinkCache cache = state.LinkCache;
+            foreach (var cellContext in state.LoadOrder.PriorityOrder.Cell().WinningContextOverrides(cache))
+            {
+                var cell = cellContext.Record;
+                if (string.IsNullOrEmpty(cell.Name?.String) || cell.EncounterZone.FormKey == null || cellContext.IsUnderneath<IWorldspaceGetter>())
+                {
+                    continue;
+                }
+                cell.EncounterZone.TryResolve(cache, out var encounterZone);
+                if (encounterZone == null) continue;
+
+                string cellName = cell.Name.String;
+                sbyte minLevel = encounterZone.MinLevel;
+                sbyte maxLevel = encounterZone.MaxLevel;
+
+                if (debugMode) Console.WriteLine($"Cell {cellName} with ECZN {minLevel} to {maxLevel}.");
+                cellCounter++;
+            }
+
+
+            Console.WriteLine($"Patched {cellCounter} Cells.");
         }
     }
 }
