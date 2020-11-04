b013021c0c5bb3da3d3624306039b3b1c3650024
Deigue <deigue@hotmail.com>
2020-11-03 19:56:05 -0500

    Added configurability for cell name-templates.

--- CellEncounterLevelsInName/Program.cs
+++ CellEncounterLevelsInName/Program.cs
@@ -5,6 +5,9 @@ using Mutagen.Bethesda;
 using Mutagen.Bethesda.Synthesis;
 using Mutagen.Bethesda.Skyrim;
 using Noggog;
+using Alphaleonis.Win32.Filesystem;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace CellEncounterLevelsInName
 {
@@ -27,10 +30,52 @@ namespace CellEncounterLevelsInName
             );
         }
 
+        private static bool ParseTemplateString(JObject jObject, string keyName, out string parsedString)
+        {
+            parsedString = "ERROR"; // Default state.
+            if (jObject.ContainsKey(keyName))
+            {
+                var parse = jObject[keyName]?
+                .ToString()
+                .Replace("{name}", "{0}")
+                .Replace("{min}", "{1}")
+                .Replace("{max}", "{2}");
+
+                if (parse != null) parsedString = parse;
+            }
+
+            return parsedString != "ERROR";
+        }
+
         public static void RunPatch(SynthesisState<ISkyrimMod, ISkyrimModGetter> state)
         {
             bool debugMode = true;
+            Console.WriteLine(); // Spaces out this patchers output.
 
+            string configFilePath = Path.Combine(state.ExtraSettingsDataPath, "config.json");
+            if (!File.Exists(configFilePath))
+            {
+                Console.WriteLine("\"config.json\" cannot be found in the users Data folder.");
+                return;
+            }
+
+            JObject config = JObject.Parse(File.ReadAllText(configFilePath));
+            string formulaRangedLeveled = "";
+            string formulaDeleveled = "";
+            string formulaLeveled = "";
+
+            if (!ParseTemplateString(config, "formulaRangedLeveled", out formulaRangedLeveled) ||
+                !ParseTemplateString(config, "formulaDeleveled", out formulaDeleveled) ||
+                !ParseTemplateString(config, "formulaLeveled", out formulaLeveled))
+            {
+                Console.WriteLine("Fields \"formulaRangedLeveled\", \"formulaDeleveled\" and \"formulaLeveled\" must be specified in \"config.json\"");
+                return;
+            }
+            
+            Console.WriteLine("*** Cell Encounter Levels In Name - Configuration ***");
+            Console.WriteLine($" formulaRangedLeveled: {formulaRangedLeveled}");
+            Console.WriteLine($" formulaDeleveled: {formulaDeleveled}");
+            Console.WriteLine($" formulaLeveled: {formulaLeveled}");
             Console.WriteLine("Running Cell Encounter Levels In Name ...");
             Console.WriteLine();
 
@@ -53,16 +98,17 @@ namespace CellEncounterLevelsInName
                 string nameTemplate;
                 if (maxLevel > minLevel)
                 {
-                    nameTemplate = "{0} ({1} ~ {2})";
-                } 
-                else if (maxLevel == minLevel) 
+                    nameTemplate = formulaRangedLeveled;
+                }
+                else if (maxLevel == minLevel)
                 {
-                    nameTemplate = "{0} ({1})";
+                    nameTemplate = formulaDeleveled;
                 }
                 else
                 {
-                    nameTemplate = "{0} ({1}+)";
+                    nameTemplate = formulaLeveled;
                 }
+
 
                 string newCellName = string.Format(nameTemplate, cellName, minLevel, maxLevel);
 
